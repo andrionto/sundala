8dd931313a2e276b3a1329ad78227e6501338da6
andrionto <andrigunawangh@gmail.com>
2022-01-16 02:46:14 +0700

    second

---
+++ app/src/main/java/com/example/sundala/FirstFragment.kt
@@ -0,0 +1,17 @@
+package com.example.sundala
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
--- app/src/main/java/com/example/sundala/MainActivity.kt
+++ app/src/main/java/com/example/sundala/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+      val firstFragment = FirstFragment()
+      val secondFragment = FirstFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R. id.flFragment, firstFragment)
+            commit()
+        }
+
     }
 }
