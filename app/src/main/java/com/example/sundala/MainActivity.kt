f8ce055b85752cfb60cf5ba3643db8149830d25f
andrionto <andrigunawangh@gmail.com>
2022-01-16 03:50:01 +0700

    second

--- app/src/main/java/com/example/sundala/MainActivity.kt
+++ app/src/main/java/com/example/sundala/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.sundala
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      val firstFragment = FirstFragment()
-      val secondFragment = FirstFragment()
+        val firstFragment = FirstFragment()
+      val secondFragment =SecondFragment()
+
 
         supportFragmentManager.beginTransaction().apply {
             replace(R. id.flFragment, firstFragment)
             commit()
+
+            }
         }
 
     }
-}
