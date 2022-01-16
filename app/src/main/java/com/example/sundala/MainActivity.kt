d73421e3b309b77d3834e4c5419943a264eb13dd
andrionto <andrigunawangh@gmail.com>
2022-01-17 06:37:32 +0700

    Commit message

--- app/src/main/java/com/example/sundala/MainActivity.kt
+++ app/src/main/java/com/example/sundala/MainActivity.kt
@@ -2,22 +2,34 @@ package com.example.sundala
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstFragment = FirstFragment()
-      val secondFragment =SecondFragment()
+        val firstFragment=FirstFragment()
+        val secondragment=SecondFragment()
 
 
         supportFragmentManager.beginTransaction().apply {
-            replace(R. id.flFragment, firstFragment)
+            replace(R.id.flFragment, firstFragment)
             commit()
-
+        }
+    btnfragment1 .setOnClickListener {
+    supportFragmentManager .beginTransaction() .apply {
+        replace(R.id.flFragment, firstFragment)
+        commit()
+    }
+}
+    btnfragment2 .setOnClickListener {
+        supportFragmentManager .beginTransaction() .apply {
+            replace(R.id.flFragment, secondragment)
+            commit()
+        }
+    }
             }
         }
 
-    }
+
